fbd4ead2d47a1884090cbf8962371ffea5239dc4
Tian Khamez <tkhamez@gmail.com>
2022-08-19 15:58:59 +0200

    Add missing table "ban" - from https://github.com/bravecollective/mumble-sso

--- create.sql
+++ create.sql
@@ -1,24 +1,31 @@
+CREATE TABLE `ticker`
+(
+    `filter` varchar(45) NOT NULL,
+    `text`   varchar(5)  NOT NULL,
+    PRIMARY KEY (`filter`)
+) DEFAULT CHARSET = UTF8MB4;
 
-CREATE TABLE `ticker` (
-  `filter` varchar(45) NOT NULL,
-  `text` varchar(5) NOT NULL,
-  PRIMARY KEY (`filter`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `user`
+(
+    `character_id`     int         NOT NULL,
+    `character_name`   varchar(45) NOT NULL,
+    `corporation_id`   int         NOT NULL,
+    `corporation_name` varchar(45) NOT NULL,
+    `alliance_id`      int         DEFAULT NULL,
+    `alliance_name`    varchar(45) DEFAULT NULL,
+    `mumble_username`  varchar(45) NOT NULL,
+    `mumble_password`  varchar(45) NOT NULL,
+    `created_at`       int         NOT NULL,
+    `updated_at`       int         NOT NULL,
+    `groups`           longtext,
+    `owner_hash`       varchar(45) NOT NULL,
+    PRIMARY KEY (`character_id`)
+) DEFAULT CHARSET = UTF8MB4;
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `character_id` int(11) NOT NULL,
-  `character_name` varchar(45) NOT NULL,
-  `corporation_id` int(11) NOT NULL,
-  `corporation_name` varchar(45) NOT NULL,
-  `alliance_id` int(11) DEFAULT NULL,
-  `alliance_name` varchar(45) DEFAULT NULL,
-  `mumble_username` varchar(45) NOT NULL,
-  `mumble_password` varchar(45) NOT NULL,
-  `created_at` int(11) NOT NULL,
-  `updated_at` int(11) NOT NULL,
-  `groups` longtext,
-  `owner_hash` varchar(45) NOT NULL,
-  PRIMARY KEY (`character_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `ban`
+(
+    `filter`          varchar(45) NOT NULL,
+    `reason_public`   longtext,
+    `reason_internal` longtext,
+    PRIMARY KEY (`filter`)
+) DEFAULT CHARSET = UTF8MB4;
